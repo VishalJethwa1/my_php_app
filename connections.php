5a8b8448335e5029e380a0d8850ecc0fd225d575
Vishal Jethwa <jethwa.vishal88@gmail.com>
2023-12-20 23:08:08 +0500

    updated connections.php

--- connections.php
+++ connections.php
@@ -3,8 +3,9 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$database = "myapp";
 
-$connection = mysqli_connect($servername, $username, $password);
+$connection = mysqli_connect($servername, $username, $password, $database);
 
 if(!$connection) {
     die("Connection Failed: ".mysqli_connect_error());
